f69d296cb94b965bb0ae2a75352be9a24739ad48
czunigar <chala926@gmail.com>
2022-04-17 16:02:07 -0600

    Updating twitter controller

--- webserver/sites/shub.xyz/controllers/twitterController.js
+++ webserver/sites/shub.xyz/controllers/twitterController.js
@@ -1,13 +1,17 @@
 const auth = require('../authentication/auth.js');
 const twitterClient = require('../authentication/twitterClient.js');
 
-const { accToken, accSecret } = auth.twitterCredentials(req.cookies.twitterAuth);
-const userTwitterClient = twitterClient.tempClient(accToken, accSecret);
 
 exports.tweet = async (req, res, next) => {
-    try {
-        await userTwitterClient.v1.tweet()
-    } catch (error) {
-        console.log(error);
+    if (req.cookies.twitterAuth) {
+        const { accToken, accSecret } = auth.twitterCredentials(req.cookies.twitterAuth);
+        const userTwitterClient = twitterClient.tempClient(accToken, accSecret);
+
+        try {
+            await userTwitterClient.v1.tweet()
+        } catch (error) {
+            console.log(error);
+        }
     }
+
 }
